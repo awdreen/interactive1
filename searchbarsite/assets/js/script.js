798e217ac331c955c3321302929ee567f2989594
Audreen <audreenhiram@gmail.com>
2018-03-12 13:48:35 -0700

    commit

--- searchbarsite/assets/js/script.js
+++ searchbarsite/assets/js/script.js
@@ -9,3 +9,7 @@ $(document).ready(function(){
 	});	
 
 });
+function newPopup(url) {
+	popupWindow = window.open(
+		url,'popUpWindow','height=300,width=400,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}
