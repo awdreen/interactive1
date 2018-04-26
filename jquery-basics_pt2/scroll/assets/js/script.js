1f50402b40e89b7ebb6910a158c2fe38feb952ea
Audreen <audreenhiram@gmail.com>
2018-04-26 12:54:56 -0700

    stuff

--- jquery-basics_pt2/scroll/assets/js/script.js
+++ jquery-basics_pt2/scroll/assets/js/script.js
@@ -1,13 +1,11 @@
 $('document').ready(function(){
-	$('img').click(function(){
-		$(this).attr('src', 'https://i.imgur.com/Z9idPCT.png');
-    $(this).addClass('bounce');
+
    $('img').click(function(){
 		$(this).attr('src', 'https://i.imgur.com/jVMROKx.png');
     $(this).addClass('bounce');
 	});
 	setTimeout(function() { 
-  	window.location.href = 'http://google.com'; 
+  	window.location.href = 'https://awdreen.github.io/interactive1/jquery-basics_pt2/scroll/index.html'; 
 	}, 2000);
 
 	
